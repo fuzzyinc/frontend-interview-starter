9f9ab885028b67651e25fbb1aec621b5b4c4738e
Andrew Nater <andrew@yourfuzzy.com>
2022-03-30 21:14:35 -0400

    Slim down repo to bare essentials

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const api = {
+  url: 'https://imdb-data-searching.p.rapidapi.com/om',
+  headers: {
+    'X-RapidAPI-Host': 'imdb-data-searching.p.rapidapi.com',
+    'X-RapidAPI-Key': '5ed7a4a7c9msh2ff7d1d421a7e25p1c7d3bjsnd573601757e2',
+  },
+};
